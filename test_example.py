bf654e0ebf75f7819f01429d4795a4aa03a7f83b
liza <lizaqa.3@gmail.com>
2022-12-02 22:56:49 +0300

    Liza test

--- test_example.py
+++ test_example.py
@@ -1,7 +1,11 @@
-def test_example():
-    assert 3 > 1
+def test_example_0():
+    assert 10 > 1
 
 another_test
     new*
-    def test_another():
-        0 == 0
+    def test_another_1():
+        0 == 1
+
+
+    def test_another_2():
+        0 == 3
